56e8956eebd9f40c4377fee1c246506af4c75052
Kerrick Cavanaugh <kerrickcavanaugh@Kerricks-MBP.domain>
2023-12-15 16:51:10 -0700

    asdf

--- examples/example1.py
+++ examples/example1.py
@@ -2,8 +2,7 @@ import neuropacs
 
 def main():
     api_key = "m0ig54amrl87awtwlizcuji2bxacjm"
-    server_url = "http://ec2-3-142-212-32.us-east-2.compute.amazonaws.com:5000"
-    # server_url = "http://localhost:5000"
+    server_url = "http://localhost:5000"
     product_id = "PD/MSA/PSP-v1.0"
     result_format = "TXT"
 
@@ -15,30 +14,24 @@ def main():
 
     # GENERATE AN AES KEY
     aes_key = npcs.generate_aes_key()
-    print(aes_key)
 
     # CONNECT TO NEUROPACS
     connection_id = npcs.connect(api_key, aes_key)
-    print(connection_id)
 
-    # # CREATE A NEW JOB
-    # order_id = npcs.new_job(connection_id, aes_key)
-    # print(order_id)
+    # CREATE A NEW JOB
+    order_id = npcs.new_job(connection_id, aes_key)
 
-    # # UPLOAD AN IMAGE
-    # npcs.upload_dataset("./dicom_examples/06_001",order_id,connection_id, aes_key)
+    # UPLOAD AN IMAGE
+    npcs.upload_dataset("./dicom_examples/06_001",order_id,connection_id, aes_key)
 
-    # # START A JOB
-    # job = npcs.run_job(product_id, order_id, connection_id, aes_key)
-    # print(job)
+    # START A JOB
+    job = npcs.run_job(product_id, order_id, connection_id, aes_key)
 
     # CHECK STATUS
-    status = npcs.check_status("TEST", connection_id , aes_key)
-    print(status)
+    status = npcs.check_status(order_id, connection_id , aes_key)
 
     # GET RESULTS
-    results = npcs.get_results(result_format, "TEST", connection_id, aes_key)
-    print(results)
+    results = npcs.get_results(result_format, order_id, connection_id, aes_key)
 
 
 main()
