2d292c0628b0480785ac784d1c3dd95f2449559d
Kerrick Cavanaugh <kerrickcavanaugh@Kerricks-MacBook-Pro.local>
2024-07-04 19:35:57 -0600

    asdf

--- neuropacs/ex.py
+++ neuropacs/ex.py
@@ -1,9 +1,9 @@
 from sdk import Neuropacs
 
 def main():
-    api_key = "kDmOadtVXC1yZfST0cqJFapIMCeKZUT2rYHBF1kd"
-    # api_key = "cdXVNIFzEUbSElTpoVoK4SyRrJ7Zj6n6Y6wgApIc"
-    server_url = "https://sl3tkzp9ve.execute-api.us-east-2.amazonaws.com/v1"
+    # api_key = "kDmOadtVXC1yZfST0cqJFapIMCeKZUT2rYHBF1kd"
+    api_key = "cdXVNIFzEUbSElTpoVoK4SyRrJ7Zj6n6Y6wgApIc"
+    server_url = "https://sl3tkzp9ve.execute-api.us-east-2.amazonaws.com/dev"
     product_id = "PD/MSA/PSP-v1.0"
     result_format = "JSON"
 
@@ -19,20 +19,20 @@ def main():
     print(conn)
 
     # # # # CREATE A NEW JOB
-    # order = npcs.new_job()
-    # print(order)
+    order = npcs.new_job()
+    print(order)
 
     # # # UPLOAD A DATASET
-    # datasetID = npcs.upload_dataset("../dicom_examples/DICOM_small", order, order, callback=lambda data: print(data))
-    # print(datasetID)
+    datasetID = npcs.upload_dataset("../dicom_examples/test_dataset", order, order, callback=lambda data: print(data))
+    print(datasetID)
 
     # # # START A JOB
     # job = npcs.run_job(product_id, "d0a8efaf-d5f0-4b4b-a600-40c477aa16eb")
     # print(job)
 
     # CHECK STATUS
-    status = npcs.check_status("d0a8efaf-d5f0-4b4b-a600-40c477aa16eb")
-    print(status)
+    # status = npcs.check_status("d0a8efaf-d5f0-4b4b-a600-40c477aa16eb")
+    # print(status)
 
     # # # GET RESULTS
     # results = npcs.get_results(result_format, "d9b5ad6c-818b-4d05-b772-80518a1f2c56")
--- tests/tests.py
+++ tests/tests.py
@@ -14,13 +14,13 @@ npcs_invalid = Neuropacs(server_url=server_url, api_key=reg_key, origin_type=ori
 
 class IntegrationTests(unittest.TestCase):
 
-    def setUp(self):
-        print("Setting up before a test")
-        # Code to set up test environment, if needed
+    # def setUp(self):
+    #     print("Setting up before a test")
+    #     # Code to set up test environment, if needed
 
-    def tearDown(self):
-        print("Cleaning up after a test")
-        # Code to clean up test environment, if needed
+    # def tearDown(self):
+    #     print("Cleaning up after a test")
+    #     # Code to clean up test environment, if needed
 
     # Successful connection
     def test_successful_connection(self):
@@ -32,6 +32,23 @@ class IntegrationTests(unittest.TestCase):
         self.assertEqual(test_utils.is_valid_timestamp(timestamp), True)
         self.assertEqual(test_utils.is_valid_uuid4(connection_id), True)
         self.assertEqual(test_utils.is_valid_aes_ctr_key(aes_key), True)
+
+    # Successful order creation
+    def test_successful_order_creation(self):
+        npcs_admin.connect()
+        order_id = npcs_admin.new_job()
+        self.assertEqual(test_utils.is_valid_uuid4(order_id), True)
+        
+    # Successful dataset upload
+    def test_successful_dataset_upload(self):
+        npcs_admin.connect()
+        order_id = npcs_admin.new_job()
+        upload_status = npcs_admin.upload_dataset("./test_dataset", order_id=order_id, dataset_id=order_id)
+        dataset_id = upload_status["dataset_id"]
+        state = upload_status["state"]
+        self.assertEqual(test_utils.is_dict(upload_status), True)
+        self.assertEqual(state, "success")
+        self.assertEqual(dataset_id, order_id)
 
     
 if __name__ == '__main__':
