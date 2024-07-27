29b1a680ba62240e51e42dc388a9fe46298126c5
Kerrick Cavanaugh <kerrickcavanaugh@Kerricks-MacBook-Pro.local>
2024-07-26 20:47:45 -0400

    asdf

--- neuropacs/ex.py
+++ neuropacs/ex.py
@@ -1,9 +1,8 @@
 from sdk import Neuropacs
 
 def main():
-    api_key = "kDmOadtVXC1yZfST0cqJFapIMCeKZUT2rYHBF1kd"
-    # api_key = "cdXVNIFzEUbSElTpoVoK4SyRrJ7Zj6n6Y6wgApIc"
-    server_url = "https://sl3tkzp9ve.execute-api.us-east-2.amazonaws.com/v1"
+    api_key = "IIhgYuScAuztZbWK54km38yc0da9him3Q3wyCuQ3"
+    server_url = "https://sl3tkzp9ve.execute-api.us-east-2.amazonaws.cov/dev"
     product_id = "PD/MSA/PSP-v1.0"
     result_format = "JSON"
 
@@ -18,24 +17,34 @@ def main():
     conn = npcs.connect()
     print(conn)
 
-    # # # # CREATE A NEW JOB
-    # order = npcs.new_job()
-    # print(order)
+    # for i in range(1000):
+    #     try:
+    npcs = Neuropacs(server_url, api_key)
 
-    # # # UPLOAD A DATASET
-    # datasetID = npcs.upload_dataset("../dicom_examples/DICOM_small", order, order, callback=lambda data: print(data))
+    # CREATE A CONNECTION   
+    conn = npcs.connect()
+    print(conn)
+        # except Exception as e:
+        #     print(str(e))
+
+    # # # # # # CREATE A NEW JOB
+    order = npcs.new_job()
+    print(order)
+
+    # # # # # UPLOAD A DATASET
+    # datasetID = npcs.upload_dataset("../dicom_examples/test_dataset", order, order, callback=lambda data: print(data))
     # print(datasetID)
 
     # # # START A JOB
-    # job = npcs.run_job(product_id, "d0a8efaf-d5f0-4b4b-a600-40c477aa16eb")
+    # job = npcs.run_job(product_id, "683abdf7-be60-4764-b35e-3bf396bdefd0")
     # print(job)
 
     # CHECK STATUS
-    status = npcs.check_status("d0a8efaf-d5f0-4b4b-a600-40c477aa16eb")
-    print(status)
+    # status = npcs.check_status("0b183dd2-d07b-4ac9-a558-1dc3d51aea80")
+    # print(status)
 
-    # # # GET RESULTS
-    # results = npcs.get_results(result_format, "d9b5ad6c-818b-4d05-b772-80518a1f2c56")
+    # # # # GET RESULTS
+    # results = npcs.get_results(result_format, "TEST")
     # print(results)
 
     
--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,5 +1,24 @@
 import json
+import datetime
+import uuid
+import base64
+import os
 
+
+# CONSTANTS
+server_url = "https://sl3tkzp9ve.execute-api.us-east-2.amazonaws.com/dev"
+invalidServerUrl = "https://invalid.execute-api.us-east-2.amazonaws.com/not_real"
+admin_key = os.getenv('ADMIN_API_KEY')
+invalid_key = "invalidApiKey123"
+reg_key = os.getenv('REG_API_KEY')
+origin_type = "Integration Tests"
+
+invalid_order_id = "invalid_order_id"
+product_id = "PD/MSA/PSP-v1.0"
+invalid_product_id = "not_a_real_product"
+
+
+# HELPER FUNCTIONS
 def is_json(myjson):
     try:
         json_object = json.loads(myjson)
@@ -9,3 +28,36 @@ def is_json(myjson):
 
 def is_dict(value):
     return isinstance(value, dict)
+
+def is_valid_timestamp(date_string):
+    try:
+        naive_datetime = datetime.strptime(date_string, "%Y-%m-%d %H:%M:%S %Z")
+    except Exception as e:
+        return False
+    return True
+
+def is_valid_uuid4(value):
+    try:
+        val = uuid.UUID(value, version=4)
+    except ValueError:
+        return False
+    return True
+
+def is_valid_aes_ctr_key(key):
+    try:
+        decoded_key = base64.b64decode(key, validate=True)
+    except (ValueError, base64.binascii.Error):
+        return False
+    return True
+
+def is_valid_status_obj(status_obj):
+  if status_obj and is_dict(status_obj) and \
+    isinstance(status_obj.get('started'), bool) and \
+    isinstance(status_obj.get('finished'), bool) and \
+    isinstance(status_obj.get('failed'), bool) and \
+    status_obj.get('progress') and \
+    isinstance(status_obj.get('progress'), int) and \
+    status_obj.get('info') and \
+    isinstance(status_obj.get('info'), str):
+        return True
+  return False
