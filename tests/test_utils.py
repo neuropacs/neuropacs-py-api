d9a30eb88b776426edf34d3299b3d5788dd2c09a
Kerrick Cavanaugh <kerrickcavanaugh@Kerricks-MacBook-Pro.local>
2024-07-30 12:14:31 -0600

    int tests

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -17,6 +17,9 @@ invalid_order_id = "invalid_order_id"
 product_id = "PD/MSA/PSP-v1.0"
 invalid_product_id = "not_a_real_product"
 
+dataset_path_local = "./test_dataset"
+dataset_path_git = "./tests/test_dataset"
+
 
 # HELPER FUNCTIONS
 def is_json(myjson):
@@ -30,13 +33,11 @@ def is_dict(value):
     return isinstance(value, dict)
 
 def is_valid_session_obj(conn):
-    if conn and \
+    return conn and \
         is_dict(conn) and \
         is_valid_timestamp(conn.get("timestamp")) and \
         is_valid_uuid4(conn.get("connection_id")) and \
-        is_valid_aes_ctr_key(conn.get('aes_key')):
-            return True
-    return False
+        is_valid_aes_ctr_key(conn.get('aes_key'))
 
 def is_valid_timestamp(date_string):
     try:
@@ -60,13 +61,22 @@ def is_valid_aes_ctr_key(key):
     return True
 
 def is_valid_status_obj(status_obj):
-  if status_obj and is_dict(status_obj) and \
+  return status_obj and is_dict(status_obj) and \
     isinstance(status_obj.get('started'), bool) and \
     isinstance(status_obj.get('finished'), bool) and \
     isinstance(status_obj.get('failed'), bool) and \
     status_obj.get('progress') and \
     isinstance(status_obj.get('progress'), int) and \
     status_obj.get('info') and \
-    isinstance(status_obj.get('info'), str):
+    isinstance(status_obj.get('info'), str)
+
+
+def is_valid_result_txt(result):
+  return isinstance(result, str) and len(result) > 0
+
+def is_valid_result_json(result):
+    try:
+        json.loads(result)
         return True
-  return False
+    except Exception as e:
+        return False
