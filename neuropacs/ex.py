47bbdc782cea668d1b83b8c67996c355ece6e59a
Kerrick Cavanaugh <kerrickcavanaugh@Kerricks-MBP.hsd1.fl.comcast.net>
2024-09-30 16:44:49 -0400

    pypi account recovery

--- neuropacs/ex.py
+++ neuropacs/ex.py
@@ -1,9 +1,9 @@
 from sdk import Neuropacs
 
 def main():
-    api_key = "Wa02MlTrzgaFHEwIZVngda916g6893M7apqvQTFc" #!DELETE THIS
+    api_key = "" #!DELETE THIS
     # server_url = "https://sl3tkzp9ve.execute-api.us-east-2.amazonaws.com/dev"
-    server_url = "https://aw75e2na5m.execute-api.us-east-1.amazonaws.com/dev"
+    server_url = "https://jdfkdttvlf.execute-api.us-east-1.amazonaws.com/prod"
     product_id = "PD/MSA/PSP-v1.0"
     result_format = "JSON"
     origin_type = "my_application"
@@ -24,24 +24,24 @@ def main():
     print(conn)
 
     # # # # CREATE A NEW JOB
-    # order = npcs.new_job()
-    # print(order)
+    order = npcs.new_job()
+    print(order)
 
     # # # # # # # UPLOAD A DATASET
-    # datasetID = npcs.upload_dataset("/Users/kerrickcavanaugh/Desktop/sample data/DICOM_small", order, order, callback=lambda data: print(data))
-    # print(datasetID)
+    datasetID = npcs.upload_dataset("/Users/kerrickcavanaugh/Desktop/sample data/DICOM_small", order, order, callback=lambda data: print(data))
+    print(datasetID)
 
-    # # # # # START A JOB
-    # job = npcs.run_job(product_id, order)
-    # print(job)
+    # # # # START A JOB
+    job = npcs.run_job(product_id, order)
+    print(job)
 
     # # # CHECK STATUS
-    status = npcs.check_status("0a90258e-e0bf-4697-bfbc-37ee6147c1ab")
+    status = npcs.check_status("TEST")
     print(status)
 
-    # # # # # GET RESULTS
-    # results = npcs.get_results("JSON", "b863e635-074b-400d-bc6f-226a15046eed")
-    # print(results)
+    # # # # GET RESULTS
+    results = npcs.get_results("JSON", "TEST")
+    print(results)
 
     
 
