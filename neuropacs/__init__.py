af0d580132308099f0761f0c7e4f8e32370b8d08
neuropacman <kerrick@neuropacs.com>
2024-10-21 22:34:12 -0400

    fix/allow higher dependency versions

--- neuropacs/__init__.py
+++ neuropacs/__init__.py
@@ -1,7 +1,7 @@
 # __init__.py
 from .sdk import Neuropacs
 
-PACKAGE_VERSION = "1.7.9"
+PACKAGE_VERSION = "1.8.0"
 
 def init(server_url, api_key, origin_type="API"):
     return Neuropacs(server_url=server_url, api_key=api_key, origin_type=origin_type)
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='neuropacs',
-    version='1.7.9',
+    version='1.8.0',
     description='neuropacs Python API',
     license='MIT',
     long_description=open('README.md').read(),
@@ -12,13 +12,9 @@ setup(
     url='https://github.com/neuropacs/neuropacs-py-sdk',
     packages=find_packages(),
     install_requires=[
-        'cryptography==41.0.1',
-        'pycryptodome==3.20.0',
-        'requests==2.31.0',
-        'rsa==4.9',
-        'urllib3==1.26.6',
-        'zipp==3.17.0',
-        'tqdm==4.66.1'
+        'cryptography>=41.0.1',
+        'pycryptodome>=3.20.0',
+        'requests>=2.31.0',
     ],
     classifiers=[
         'Development Status :: 4 - Beta',
@@ -28,7 +24,7 @@ setup(
     ],
 )
 
-# To update this SDK:
+# To update this package:
 # 1. Remove /build /dist /neuropacs.egg-info
 # 2. Update version in setup.py and __init__.py
 # 3. Run: python setup.py sdist bdist_wheel
