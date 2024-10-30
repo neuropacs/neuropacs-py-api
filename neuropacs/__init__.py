aa28c959daf495d743f920fb4909368018696cc7
neuropacman <kerrick@neuropacs.com>
2024-10-30 16:27:37 -0400

    gh-pages

--- neuropacs/__init__.py
+++
@@ -1,9 +0,0 @@
-# __init__.py
-from .sdk import Neuropacs
-
-PACKAGE_VERSION = "1.8.1"
-
-def init(server_url, api_key, origin_type="API"):
-    return Neuropacs(server_url=server_url, api_key=api_key, origin_type=origin_type)
-
-
